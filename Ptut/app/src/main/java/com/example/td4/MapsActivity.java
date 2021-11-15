fbb0cf0b6ec89d24140265650f7e5a4c253ad73d
Sataross <mathieu.lebrette@gmail.com>
2021-11-15 10:27:23 +0100

    des changements

--- Ptut/app/src/main/java/com/example/td4/MapsActivity.java
+++ Ptut/app/src/main/java/com/example/td4/MapsActivity.java
@@ -26,6 +26,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, LocationListener {
@@ -72,10 +73,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap = googleMap;
 
-       /* googleMap.setMapStyle(
-                MapStyleOptions.loadRawResourceStyle(
-                        this, R.raw.style_json));
-*/
 
         /**Retire la possibilité de zoomer et naviguer sur la map à l'utilisateur**/
 
@@ -186,7 +183,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 break;
         }
 
-        finish();
+
 
 
     }
--- Ptut/app/src/main/java/com/example/td4/present.java
+++ Ptut/app/src/main/java/com/example/td4/present.java
@@ -8,8 +8,6 @@ public abstract class present extends MapsActivity{
 
     //Permet de changer d'ecran pour passer a l'ecran des informations de batiment
     public void change(){
-        Intent intent= new Intent(getApplicationContext(),MapsActivity.class);
-        startActivity(intent);
         finish();
     }
 }
