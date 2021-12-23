7ff73c6309bed565e7641aa55f2f4fc028e647a2
Sataross <mathieu.lebrette@gmail.com>
2021-12-23 16:05:42 +0100

    ajout truc pété mais en vrai j'en avais envie
    la class abstract game instancie un choix de verification pour quittez un jeu en cours(a ajoutez sur chaque jeu)

--- Ptut/app/src/main/java/com/example/td4/game.java
+++ Ptut/app/src/main/java/com/example/td4/game.java
@@ -1,13 +1,33 @@
 package com.example.td4;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.fragment.app.FragmentActivity;
 
 public abstract class game extends FragmentActivity {
 
     public boolean state;
+    public View verifPage;
+    public Button annuler;
+    public Button accepter;
+
+
 
     public void setState(boolean state){
         this.state=state;
     }
+
+    @Override
+    public void onBackPressed(){
+        verifPage=findViewById(R.id.verifPage);
+        annuler=findViewById(R.id.annuler);
+        annuler.setOnClickListener(v -> verifPage.setVisibility(View.INVISIBLE));
+        accepter=findViewById(R.id.accepter);
+        accepter.setOnClickListener(v -> finish());
+        verifPage.setVisibility(View.VISIBLE);
+    }
+
 
 }
