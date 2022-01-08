1f15634bc529734b0a52502ee83dc1cb7988ee7c
Olivia1901 <olivia.manceau18@gmail.com>
2022-01-08 20:33:23 +0100

    jai re tout changé

--- Ptut/app/src/main/java/com/example/td4/GBActivity.java
+++ Ptut/app/src/main/java/com/example/td4/GBActivity.java
@@ -2,13 +2,14 @@ package com.example.td4;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class GBActivity extends Present {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gb);
-        final Button button = findViewById(R.id.button1);
+        final ImageButton button = findViewById(R.id.button1);
         button.setOnClickListener(v -> change());
     }
 
