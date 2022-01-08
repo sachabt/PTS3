7ad4d1b1d44e2845c76f526e9d6989873ec39b26
saeldris <77985784+saeldris@users.noreply.github.com>
2022-01-09 00:32:26 +0100

    fin de journée

--- Ptut/app/src/main/java/com/example/td4/GBGame.java
+++ Ptut/app/src/main/java/com/example/td4/GBGame.java
@@ -2,36 +2,71 @@ package com.example.td4;
 
 import android.app.Activity;
 import android.content.ClipData;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class GBGame extends Activity {
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GBGame extends Present {
 
     ImageView[] ingredients;
     ConstraintLayout general;
     ImageView add;
+    List<ImageView> proposition;
+    Button valider, cacher;
+    int[] recette;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gbgame);
+
+        recette = new int[]{(int)(Math.random() * 5), (int)(Math.random() * 5), (int)(Math.random() * 5)};
+        proposition = new ArrayList<>();
+
+        createDialog(recette);
+
+        valider = findViewById(R.id.valider);
+        valider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(proposition.size() == 3){
+                    for(int i = 0; i < 4; i++){
+                        if(ingredients[recette[i]].getId() != proposition.get(i).getId()){
+                            //cperdu
+                        }
+                    }
+                    //cgagné
+                }
+                //cperdu
+            }
+        });
 
         add = findViewById(R.id.add);
         add.setOnDragListener(new MyDragListener());
 
         general = findViewById(R.id.general);
 
+        general.setBackgroundResource(R.drawable.background_gb);
+
         ingredients = new ImageView[5];
         ingredients[0] = findViewById(R.id.aile);
         ingredients[1] = findViewById(R.id.bave);
-        ingredients[2] = findViewById(R.id.grife);
+        ingredients[2] = findViewById(R.id.griffe);
         ingredients[3] = findViewById(R.id.corne);
         ingredients[4] = findViewById(R.id.oreille);
 
@@ -84,17 +119,16 @@ public class GBGame extends Activity {
                     owner.removeView(view);
                     container.addView(view);
                     //view.setVisibility(View.VISIBLE);
-                    for(int i = 0; i < 5; i++){
-                        if(ingredients[i] == view){
-                            switch(i){
-                                case 0 : general.setBackgroundResource(R.drawable.background_gb1);
-                                case 1 : general.setBackgroundResource(R.drawable.background_gb2);
-                                case 2 : general.setBackgroundResource(R.drawable.background_gb3);
-                                case 3 : general.setBackgroundResource(R.drawable.background_gb4);
-                                case 4 : general.setBackgroundResource(R.drawable.background_gb5);
-                                default : general.setBackgroundResource(R.drawable.background_gb);
-                            }
-                        }
+
+                    proposition.add((ImageView) view);
+
+                    switch(view.getId()){
+                        case R.id.aile : general.setBackgroundResource(R.drawable.background_gb1);break;
+                        case R.id.griffe : general.setBackgroundResource(R.drawable.background_gb2);break;
+                        case R.id.bave : general.setBackgroundResource(R.drawable.background_gb3);break;
+                        case R.id.corne : general.setBackgroundResource(R.drawable.background_gb4);break;
+                        case R.id.oreille : general.setBackgroundResource(R.drawable.background_gb5);break;
+                        default:break;
                     }
                     break;
                 default:
@@ -103,4 +137,35 @@ public class GBGame extends Activity {
             return true;
         }
     }
+
+    public void createDialog(int[] recette){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Voici la recette");
+
+        String ingrédients = "Tu aura besoin ";
+
+        for(int i = 0; i < 3; i++) {
+            switch (recette[i]) {
+                case 0:
+                    ingrédients += "d'une aile, ";
+                    break;
+                case 1:
+                    ingrédients += "de bave de crapaud, ";
+                    break;
+                case 2:
+                    ingrédients += "d'une griffe de chat, ";
+                    break;
+                case 3:
+                    ingrédients += "d'une corne de licorne, ";
+                    break;
+                case 4:
+                    ingrédients += "d'une oreille d'humain, ";
+                    break;
+            }
+        }
+
+        builder.setMessage(ingrédients);
+        builder.create().show();
+    }
+
 }
