38c3dfb2a6362d47032201d6233c3c0145f9b979
Sataross <mathieu.lebrette@gmail.com>
2022-01-10 17:23:24 +0100

    ajout colorIcon

--- Ptut/app/src/main/java/com/example/td4/Present.java
+++ Ptut/app/src/main/java/com/example/td4/Present.java
@@ -1,10 +1,18 @@
 package com.example.td4;
 
+import android.content.Intent;
+
 public abstract class Present extends MapsActivity{
 
 
     //Permet de changer d'ecran pour passer a l'ecran des informations de batiment
     public void change(){
         finish();
+    }
+
+    @Override
+    public void onBackPressed(){
+        Intent map= new Intent(getApplicationContext(),MapsActivity.class);
+        startActivity(map);
     }
 }
