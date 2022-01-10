84287c74928654f31dd7df037d6ff665e96e9bb1
Sataross <mathieu.lebrette@gmail.com>
2022-01-10 17:56:36 +0100

    poussez madame

--- Ptut/app/src/main/java/com/example/td4/MMIActivity.java
+++ Ptut/app/src/main/java/com/example/td4/MMIActivity.java
@@ -1,15 +1,23 @@
 package com.example.td4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MMIActivity extends Present {
+    private Button play;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mmi);
         ImageView backButton = this.findViewById(R.id.backButtonT);
         backButton.setOnClickListener(v -> change());
+        play=this.findViewById(R.id.startT);
+        play.setOnClickListener(v -> {
+            Intent mmiGame=new Intent(getApplicationContext(),MMIGameFirstPart.class);
+            startActivity(mmiGame);
+        });
     }
 
 }
